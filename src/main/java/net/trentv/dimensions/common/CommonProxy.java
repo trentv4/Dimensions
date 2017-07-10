a088231da7150fffca2eaa6aea3cd95fafbb54b1
trentv4 <trentvanslyke@gmail.com>
2017-07-10 14:01:37 -0400

    Implement library-of-babel UI on all bookcases (smoldering, charred, marmor, normal, wet, soaked)

--- src/main/java/net/trentv/dimensions/common/CommonProxy.java
+++ src/main/java/net/trentv/dimensions/common/CommonProxy.java
@@ -1,13 +1,21 @@
 package net.trentv.dimensions.common;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.trentv.dimensions.client.GuiScreenBookOutput;
 import net.trentv.dimensions.common.libraria.DimensionLibraria;
+import net.trentv.dimensions.common.libraria.LibrariaObjects;
 
 public abstract class CommonProxy
 {
@@ -18,6 +26,8 @@ public abstract class CommonProxy
 
 	public static class CommonEvents
 	{
+		private char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ',', ' ', '.' };
+
 		@SubscribeEvent
 		public void registerBlocks(RegistryEvent.Register<Block> event)
 		{
@@ -44,6 +54,43 @@ public abstract class CommonProxy
 			{
 				event.getRegistry().register(itemBlock);
 			}
+		}
+
+		@SubscribeEvent
+		public void onRightClick(RightClickBlock event)
+		{
+			BlockPos pos = event.getPos();
+			Block block = event.getWorld().getBlockState(pos).getBlock();
+			if (block == Blocks.BOOKSHELF | block == LibrariaObjects.CHARRED_BOOKSHELF | block == LibrariaObjects.SMOLDERING_BOOKSHELF | block == LibrariaObjects.WET_BOOKSHELF | block == LibrariaObjects.SOAKED_BOOKSHELF | block == LibrariaObjects.MARMOR_BOOKSHELF)
+			{
+				Random random = new Random();
+
+				random.setSeed(seedify(pos.getX() + "," + pos.getY() + "," + pos.getZ()));
+				String s = "";
+				for (int i = 0; i < 300; i++)
+				{
+					s += alphabet[random.nextInt(alphabet.length)];
+				}
+				if (event.getWorld().isRemote)
+				{
+					Minecraft.getMinecraft().displayGuiScreen(new GuiScreenBookOutput(s));
+				}
+				event.setCanceled(true);
+			}
+		}
+
+		private long seedify(String s)
+		{
+			if (s != null)
+			{
+				long hash = 0;
+				for (char c : s.toCharArray())
+				{
+					hash = 31L * hash + c;
+				}
+				return hash;
+			}
+			return 0;
 		}
 	}
 }
