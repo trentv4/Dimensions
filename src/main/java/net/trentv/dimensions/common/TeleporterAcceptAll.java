c7be2a07e0ab0a31c02d61dbc225b9e915c21223
trentv4 <trentvanslyke@gmail.com>
2018-07-20 18:50:35 -0400

    Add more correct destination choosing to books

--- src/main/java/net/trentv/dimensions/common/TeleporterAcceptAll.java
+++ src/main/java/net/trentv/dimensions/common/TeleporterAcceptAll.java
@@ -1,14 +1,35 @@
 package net.trentv.dimensions.common;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
+import net.trentv.dimensions.common.libraria.DimensionLibraria;
+import net.trentv.dimensions.common.smudged.DimensionSmudgedCity;
 
 public class TeleporterAcceptAll extends Teleporter
 {
 	public TeleporterAcceptAll(WorldServer world)
 	{
 		super(world);
+	}
+
+	@Override
+	public void placeInPortal(Entity entity, float rotationYaw)
+	{
+		if (entity instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer) entity;
+			BlockPos position = player.getBedLocation() == null ? entity.world.getSpawnPoint() : player.getBedLocation();
+
+			if (player.dimension == DimensionSmudgedCity.dimensionID)
+				position = new BlockPos(0, 128, 0);
+			if (player.dimension == DimensionLibraria.dimensionID)
+				position = new BlockPos(72, 98, 72);
+
+			player.setPosition(position.getX(), position.getY(), position.getZ());
+		}
 	}
 
 	@Override
