36873e9c714aa9bcc2b276e8963a4a2fb338258b
trentv4 <trentvanslyke@gmail.com>
2018-07-26 09:26:04 -0400

    Fix doors not placing with both upper and lower

--- src/main/java/net/trentv/dimensions/common/libraria/block/BlockMarmorDoor.java
+++ src/main/java/net/trentv/dimensions/common/libraria/block/BlockMarmorDoor.java
@@ -5,9 +5,12 @@ import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.trentv.dimensions.common.libraria.LibrariaObjects;
 
 public class BlockMarmorDoor extends BlockDoor
@@ -15,6 +18,7 @@ public class BlockMarmorDoor extends BlockDoor
 	public BlockMarmorDoor(Material materialIn)
 	{
 		super(materialIn);
+		this.useNeighborBrightness = true;
 	}
 
 	@Override
@@ -27,5 +31,12 @@ public class BlockMarmorDoor extends BlockDoor
 	public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos)
 	{
 		return MapColor.WHITE_STAINED_HARDENED_CLAY;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer()
+	{
+		return BlockRenderLayer.SOLID;
 	}
 }
